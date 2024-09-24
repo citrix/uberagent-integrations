d2b799c57f31b2a4c144fdd4514861409102282c
Akhil Balakrishnan <akhil.balakrishnan@cloud.com>
2024-09-24 16:21:28 +0530

    Updated the powershell script to download the files from repo instead of using local copes. Made it support update by hardcoding the IDs

--- azure-monitor/azure-workbooks/deploy-workbooks.ps1
+++ azure-monitor/azure-workbooks/deploy-workbooks.ps1
@@ -1,13 +1,31 @@
+param (
+    [string]$SubscriptionId,
+    [string]$WorkspaceName,
+    [string]$ResourceGroup
+)
+
+function DownloadFiles{ 
+    param (
+        [string[]]$OriginalFiles
+    )
+    foreach ($FileName in $OriginalFiles) {
+        $Url = "https://raw.githubusercontent.com/citrix/uberagent-integrations/refs/heads/main/azure-monitor/azure-workbooks/arm-templates/$FileName"
+        $Output = $FileName
+        Invoke-WebRequest -Uri $url -OutFile $Output
+        Write-Host "Downloaded $FileName"
+    }
+}
+
 function CopyFiles{ 
     param (
-        [string[]]$originalFiles,
-        [string]$newPrefix
+        [string[]]$OriginalFiles,
+        [string]$NewPrefix
     )
-    foreach ($fileName in $originalFiles) {
-        $newFileName = $newPrefix + $fileName
-        $origFilePath = "arm-templates/" + $fileName
-        Copy-Item -Path $origFilePath -Destination $newFileName
-        Write-Host "Copied $fileName to $newFileName"
+    foreach ($FileName in $OriginalFiles) {
+        $NewFileName = $NewPrefix + $FileName
+        $OrigFilePath = $FileName
+        Copy-Item -Path $OrigFilePath -Destination $NewFileName
+        Write-Host "Copied $FileName to $NewFileName"
     }
 }
 
@@ -15,78 +33,106 @@ function CopyFiles{
 
 function DeleteFiles{
     param (
-        [string[]]$originalFiles,
-        [string]$newPrefix
+        [string[]]$OriginalFiles,
+        [string]$NewPrefix
     )
-    foreach ($fileName in $originalFiles) {
-        $fileToDelete = $newPrefix + $fileName
-        if (Test-Path $fileToDelete) {
-            Remove-Item -Path $fileToDelete -Force
-            Write-Host "File '$fileToDelete' deleted successfully."
+    foreach ($FileName in $OriginalFiles) {
+        $CopyFileToDelete = $NewPrefix + $FileName
+        if (Test-Path $FileName) {
+            Remove-Item -Path $FileName -Force
+            Write-Host "File '$FileName' deleted successfully."
         } else {
-            Write-Host "File '$fileToDelete' does not exist."
+            Write-Host "File '$FileName' does not exist."
+        }
+        if (Test-Path $CopyFileToDelete) {
+            Remove-Item -Path $CopyFileToDelete -Force
+            Write-Host "File '$CopyFileToDelete' deleted successfully."
+        } else {
+            Write-Host "File '$CopyFileToDelete' does not exist."
         }
     }
 }
 
 function ReplaceDrilldownLink{
     param (
-        [string]$fileName,
-        [string]$workbookId,
-        [string]$resourceGroup,
-        [string]$subscriptionId
+        [string]$FileName,
+        [string]$WorkbookId,
+        [string]$ResourceGroup,
+        [string]$SubscriptionId
     )
-    $workbooksourceId = "/subscriptions/"+$subscriptionId+"/resourceGroups/"+$resourceGroup+"/providers/microsoft.insights/workbooks/"+$workbookId
-    $fileContent = Get-Content -Path $fileName
-    $newContent = $fileContent.Replace("<<<REPLACEHERE>>>", $workbooksourceId)
-    Set-Content -Path $fileName -Value $newContent
+    $WorkbooksourceId = "/subscriptions/"+$SubscriptionId+"/resourceGroups/"+$ResourceGroup+"/providers/microsoft.insights/workbooks/"+$workbookId
+    $FileContent = Get-Content -Path $FileName
+    $NewContent = $FileContent.Replace("<<<REPLACEHERE>>>", $WorkbooksourceId)
+    Set-Content -Path $FileName -Value $NewContent
 
 }
 
 
 function importFiles{
     param (
-        [string]$subscriptionId,
-        [string]$resourceGroup,
-        [string]$workspaceName,
-        [string[]]$originalFileNames,
-        [string]$newPrefix
+        [string]$SubscriptionId,
+        [string]$ResourceGroup,
+        [string]$WorkspaceName,
+        [string[]]$OriginalFileNames,
+        [string[]]$WorkbookIds,
+        [string]$NewPrefix
     )
     
-    for ($i = 0; $i -lt $originalFileNames.Length; $i++) {
-        $fileName = $newPrefix+$originalFileNames[$i]
-        $workbooksourceId = "/subscriptions/"+$subscriptionId+"/resourceGroups/"+$resourceGroup+"/providers/Microsoft.OperationalInsights/workspaces/"+$workspaceName
-        New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup `
-            -TemplateFile $fileName `
+    for ($i = 0; $i -lt $OriginalFileNames.Length; $i++) {
+        $FileName = $NewPrefix+$OriginalFileNames[$i]
+        $WorkbookId = $WorkbookIds[$i]
+        $workbooksourceId = "/subscriptions/"+$SubscriptionId+"/resourceGroups/"+$ResourceGroup+"/providers/Microsoft.OperationalInsights/workspaces/"+$WorkspaceName
+        New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup `
+            -TemplateFile $FileName `
             -TemplateParameterObject @{
                 "workbookSourceId" = $workbookSourceId
+                "workbookId" = $workbookId
             }
     }
 }
 
-#Reading the parameters from the user
-$subscriptionId = Read-Host "Enter your Azure subscription ID"
-$workspaceName = Read-Host "Enter the Log Analytics workspace name"
-$resourceGroup = Read-Host "Enter the resourceGroup name"
-
-Set-AzContext $subscriptionId
-
-#If you need more files to be imported, change here
-$originalFileNames = @("citrix_application_usage.json", "citrix_machines.json", "citrix_session_configuration_details.json", "citrix_single_machine_detail.json", "citrix_session_single_user_detail.json")
-
-#If you need more files to be replaced, change here. Please make sure the order is same in file names and ids
-$replacementNeededFileNames = @( 'citrix_machines.json', 'citrix_session_configuration_details.json')
-$replacementNeededIds = @("a2b60e6a-a702-4703-92ff-3a0bbed5a028", "ab17cd9b-b615-4dff-9215-570bcae0c031")
-
-$newPrefix = "temp_"
-
-CopyFiles $originalFileNames $newPrefix;
-
-for ($i = 0; $i -lt $replacementNeededFileNames.Length; $i++) {
-    $fileName = $newPrefix+$replacementNeededFileNames[$i]
-    $id = $replacementNeededIds[$i]
-    ReplaceDrilldownLink -fileName $fileName -workbookId $id -subscriptionId $subscriptionId -resourceGroup $resourceGroup -newPrefix $newPrefix
+#check if the user has already passed the details as cli arguments. If not, prompt the user to enter the details
+if (-not $subscriptionId) {
+    $subscriptionId = Read-Host "Enter your Azure subscription ID: "
 }
 
-importFiles -subscriptionId $subscriptionId -workspaceName  $workspaceName -resourceGroup $resourceGroup -originalFileNames $originalFileNames -newPrefix $newPrefix
-DeleteFiles $originalFileNames $newPrefix;
+if (-not $workspaceName) {
+    $workspaceName = Read-Host "Enter the Log Analytics workspace name"
+}
+
+if (-not $resourceGroup) {
+    $resourceGroup = Read-Host "Enter the resourceGroup name"
+}
+
+Set-AzContext $SubscriptionId
+
+
+#************************ Edit here when you want to add more templates ********************************
+#The list of arm template files of the workbooks we need to import. The IDs will be taken from this array
+#DO NOT CHANGE THE ORDER OF THE FILES AND IDS
+$OriginalFileNames = @("citrix_application_usage.json", "citrix_machines.json", "citrix_session_configuration_details.json", "citrix_single_machine_detail.json", "citrix_session_single_user_detail.json")
+$WorkbookIds = @("e000970a-6da8-45a2-8265-eebc7709b0a1", "e000970a-6da8-45a2-8265-eebc7709b08f", "e000970a-6da8-45a2-8265-eebc7709b02e", "2b17cd9b-b615-4dff-9215-570bcae0c031", "c2b60e6a-a702-4703-92ff-3a0bbed5a028")
+
+# This part is used for linking different workbooks. If you need to link a workbook to another workbook, 
+# you can add the source workbook name and destination workbook ID here. Make 
+$ReplacementNeededFileNames = @( 'citrix_machines.json', 'citrix_session_configuration_details.json')
+$ReplacementNeededIds = @("2b17cd9b-b615-4dff-9215-570bcae0c031", "c2b60e6a-a702-4703-92ff-3a0bbed5a028")
+#******************************************************************************************************
+
+
+
+$NewPrefix = "temp_"
+
+DownloadFiles -OriginalFiles $OriginalFileNames
+CopyFiles $originalFileNames $newPrefix;
+
+for ($i = 0; $i -lt $ReplacementNeededFileNames.Length; $i++) {
+    $FileName = $NewPrefix+$ReplacementNeededFileNames[$i]
+    $id = $ReplacementNeededIds[$i]
+    ReplaceDrilldownLink -FileName $FileName -WorkbookId $id -SubscriptionId $SubscriptionId -ResourceGroup $ResourceGroup -NewPrefix $NewPrefix
+}
+
+importFiles -SubscriptionId $SubscriptionId -WorkspaceName  $WorkspaceName -ResourceGroup $ResourceGroup -OriginalFileNames $OriginalFileNames -WorkbookIds $WorkbookIds -NewPrefix $NewPrefix
+DeleteFiles $OriginalFileNames $NewPrefix;
+
+Write-Host "Deployment Completed. Please check the workbooks in the Azure portal if you are not seeing any explicit error message in terminal. It may take uip to 15 mins for the workbooks to appear in the portal."
